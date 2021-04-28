918559390bc5256bf1f23d36d076bd4ac5c2720f
Peter Moore <13384523+legistek@users.noreply.github.com>
2021-04-28 13:01:14 -0500

    Delete node_modules/@antimatterjs directory

--- node_modules/@antimatterjs/react/src/BindableProp.tsx
+++
@@ -1,7 +0,0 @@
-import { BindingMode } from "./BindingParameters";
-
-export class BindableProp
-{
-    public Name: string = '';
-    public DefaultBindingMode: BindingMode = BindingMode.OneWay;
-}
