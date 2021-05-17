7685e716aba7cf1c75f9837de2d9134565a9885f
Peter N. Moore <pmoore@legistek.com>
2021-05-17 14:29:03 -0500

    More better css

--- @antimatterjs/positron/src/FrameworkElement.tsx
+++ @antimatterjs/positron/src/FrameworkElement.tsx
@@ -93,7 +93,7 @@ export class FrameworkElement<
                     this.state.IsLoading && this.state.LoadingTemplate
                         ? this.state.LoadingTemplate()
                         : (this.state.ToolTip
-                            ? (<TooltipHost content={this.state.ToolTip}>
+                            ? (<TooltipHost content={this.state.ToolTip} closeDelay={60000}>
                                     { this.renderElement()}
                                 </TooltipHost>)
                             : this.renderElement())
