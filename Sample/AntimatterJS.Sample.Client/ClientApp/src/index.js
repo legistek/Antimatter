58480fe0256699195b548b8e09b20918a3ba9262
Peter N. Moore <pmoore@legistek.com>
2021-06-29 11:49:52 -0500

    restore company sample app

--- Sample/AntimatterJS.Sample.Client/ClientApp/src/index.js
+++ Sample/AntimatterJS.Sample.Client/ClientApp/src/index.js
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, HashRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter, Route } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
 import { Antimatter, SignalRServer, ReactClient, WebassemblyServer, DataContext } from '@antimatterjs/react';
@@ -63,20 +63,19 @@ loadTheme(theme);
 (async function ()
 {
     await Antimatter.StartAsync(
-        null, //new WebassemblyServer(),
+        new WebassemblyServer(),
         new ReactClient());
 
-    //var appModel = await Antimatter.Server.GetRootObject("app");
-    //var limine = await Antimatter.Server.GetRootObject("limine");
-
+    var appModel = await Antimatter.Server.GetRootObject("app");
+    
     const baseUrl = document.getElementsByTagName('base')[0].getAttribute('href');
     const rootElement = document.getElementById('root');
 
     ReactDOM.render(
         <div>
             <BrowserRouter basename={baseUrl}>                
-                <ViewerTestApp />                
-                {/*<App Model={appModel}/>*/}                
+                {/*<ViewerTestApp />      */}          
+                <App Model={appModel}/>
             </BrowserRouter>
         </div>,
         rootElement);
