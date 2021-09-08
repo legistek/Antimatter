cdb1fea6f7898b61ac102e3a308c7cca2ba15e25
wwilliford <wwilliford@legistek.com>
2021-09-08 09:55:40 -0500

    Removed some old residual components

--- Sample/AntimatterJS.Sample.Client/ClientApp/src/components/Company.tsx
+++ Sample/AntimatterJS.Sample.Client/ClientApp/src/components/Company.tsx
@@ -10,7 +10,6 @@ import
     MessageBarType,
 
     ToggleButton,
-    Coachmark,
     TeachingBubble,
 
     ProgressBar,
@@ -71,9 +70,9 @@ export class Employee extends AntimatterComponent<{ Value: ModelObjectReference 
     {
         // amx-grow-entrance
         return (
-            <GroupBox 
+            <GroupBox
             /*animation: `${MotionAnimations.slideDownIn.replace("100ms", "400ms")}, ${MotionAnimations.fadeIn.replace("100ms", "400ms")}`*/
-            >                
+            >
                 <DataContext Value={this.state.Value}>
                     <StackPanel>
 
@@ -209,7 +208,7 @@ export class Employee extends AntimatterComponent<{ Value: ModelObjectReference 
 
                         </StackPanel>
                 </DataContext>
-                
+
             </GroupBox>
         );
     }
@@ -594,7 +593,7 @@ export class Company extends FrameworkElement<IFrameworkElementProps, IFramework
                 />
                 {/*
                 <div className="amx-ptn-fe" style={{ height: 1024 }}>
-                    
+
                 </div>
 
 
